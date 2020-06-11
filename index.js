62b9ce8cf12a9f17a6f55a2970626ff1a8055def
Krystal Heard <60255089+krocrr@users.noreply.github.com>
2020-06-11 17:37:23 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+  // wait till HTML loads then run this { } JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+    button.addEventListener('click', function () mOver(obj) {
+      obj.innerHTML = "$300 Dead Stock"
+    }
+function mOut(obj) {
+      obj.innerHTML = "Buy Now"
+    }
+    function mOver(obj) {
+      obj.innerHTML = "$600 Dead Stock"
+    }
+
+    function mOut(obj) {
+      obj.innerHTML = "Buy Now"
+    }
+    function mOver(obj) {
+      obj.innerHTML = "$190 Dead Stock"
+    }
+
+    function mOut(obj) {
+      obj.innerHTML = "Buy Now"
+    }
+    console.log(testfile)
+    });
+  });
