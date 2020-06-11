e027cc21302db737face2b3c6ace72413b1c68a2
Krystal Heard <60255089+krocrr@users.noreply.github.com>
2020-06-11 17:32:55 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    button.addEventListener('click', function () mOver(obj) {
+      obj.innerHTML = "$300 Dead Stock"
+    }
+function mOut(obj) {
+      obj.innerHTML = "Buy Now"
+    }
+    function mOver(obj) {
+      obj.innerHTML = "$600 Dead Stock"
+    }
+
+    function mOut(obj) {
+      obj.innerHTML = "Buy Now"
+    }
+    function mOver(obj) {
+      obj.innerHTML = "$190 Dead Stock"
+    }
+
+    function mOut(obj) {
+      obj.innerHTML = "Buy Now"
+    }
+    console.log(testfile)
+    });
+  });
